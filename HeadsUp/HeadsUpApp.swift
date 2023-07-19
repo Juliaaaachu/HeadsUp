3a6ec8a2933e2c742991aa330ae2e02f96664f97
Juliaaaachu <juliachu910125@gmail.com>
2023-07-18 21:40:52 -0500

    Initial Commit

---
+++ HeadsUp/HeadsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadsUpApp.swift
+//  HeadsUp
+//
+//  Created by Julia Chu on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HeadsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
