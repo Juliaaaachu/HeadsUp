65acc4adb69d85d0f03d3d3e560859a49ab0db33
Juliaaaachu <juliachu910125@gmail.com>
2023-07-19 19:20:46 -0500

    initial commit

---
+++ HeadsUp/Home.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  HeadsUp
+//
+//  Created by Julia Chu on 7/18/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            Text("hi")
+            Text("hi")
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
